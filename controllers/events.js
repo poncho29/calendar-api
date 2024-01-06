5f72bf78646333a93d4b68b636e83f96f315cf30
Sebastian Meneses <meneses0828@gmail.com>
2024-01-05 21:35:51 -0500

    [ADD] - update event

--- controllers/events.js
+++ controllers/events.js
@@ -12,6 +12,7 @@ const createEvent = async (req, res = response) => {
     res.json({
       ok: true,
       event: newEvent,
+      msg: 'Evento creado correctamente'
     })
   } catch (error) {
     console.log(error);
@@ -22,10 +23,12 @@ const createEvent = async (req, res = response) => {
   }
 }
 
-const getEvents = (req, res = response) => {
+const getEvents = async (req, res = response) => {
+  const events = await Event.find().populate('user', 'name');
+
   res.json({
     ok: true,
-    msg: 'Get Events'
+    events
   });
 }
 
@@ -36,11 +39,48 @@ const getEvent = (req, res = response) => {
   });
 }
 
-const updateEvent = (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: 'Update Event',
-  });
+const updateEvent = async (req, res = response) => {
+  const eventId = req.params.id;
+  const uid = req.uid;
+
+  try {
+    const event = await Event.findById(eventId);
+
+    if (!event) {
+      res.status(404).json({
+        ok: false,
+        msg: 'El evento no existe'
+      })
+    }
+
+    if (event.user.toString() !== uid) {
+      res.status(401).json({
+        ok: false,
+        msg: 'No tiene permiso de editar este evento'
+      });
+    }
+
+    const newEvent = {
+      ...req.body,
+      user: uid,
+    }
+
+    const updateEvent = await Event.findByIdAndUpdate(eventId, newEvent, {
+      new: true
+    });
+
+    res.json({
+      ok: true,
+      event: updateEvent,
+      msg: 'Evento actulizado correctamente',
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Fallo la actualización del evento'
+    })
+  }
 }
 
 const deleteEvent = (req, res = response) => {
